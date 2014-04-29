8fcd9f3daa6b911591129bfe47e7d83b174ba26e
RudyEscalera <morbocuby@hotmail.com>
2014-04-29 08:41:32 -0400

    arreglando segundo

--- coordinador_vistas.rb
+++ coordinador_vistas.rb
@@ -3,9 +3,6 @@ require 'sinatra'
 
 get '/' do
   @empleados = RepositorioEmpleado.instance.recuperarEmpleados
-   @empleados.each do |empleado| 
-     RepositorioJson.instance.adiccionarEmpleado(empleado)
-   end
   erb :"empleados/lista_empleados"
 end
 
